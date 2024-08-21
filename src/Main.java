601a8185072af0de055c8e4ba3f7610f56a832ee
JorgeCampiol <jorgecampiol18@gmail.com>
2024-08-21 20:46:03 -0300

    Criação do projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import jdk.net.UnixDomainPrincipal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer IdadePessoa =20;
+        String NomePessoa = "Taffezinho";
+        /*
+        * Operadores Logicos
+        * > maior
+        * >= maior igual
+        * < menor
+        * <= menor  igual
+        * == éigual
+        * != é diferente
+        * equal compara textos
+        * */
+
+        if(IdadePessoa >= 21){}
+        System.out.println("é maior que 21");
+
+    }else{
+        System.out.println("não é maior");
     }
 }
